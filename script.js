5aef3cf1a6088d5737438f7112dd52431b33df2e
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2021-11-28 15:48:15 -0300

    adding icons for inputs

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-var qtdCarne
-var qtdAgua
-var qtdRefri
-var qtdCerveja
-
 function calcular(){
-    let nAdultos = parseFloat(document.getElementById("nAdultos").value)
-    let nCriancas = parseFloat(document.getElementById("nCriancas").value)
+    let nAdultos = parseInt(document.getElementById("nAdultos").value)
+    let nCriancas = parseInt(document.getElementById("nCriancas").value)
     let duracao = parseFloat(document.getElementById("duracao").value)
     qtdCarne = ((nAdultos*2+nCriancas)*duracao)/10
     qtdAgua = Math.ceil(((nAdultos*2+nCriancas)*duracao)/10)
@@ -17,16 +12,16 @@ function calcular(){
 function limparPag(){
     let carne = document.getElementById("carne")
     carne.style.display = "inline-block"
-    carne.innerHTML = ("A quantidade de carne é:")
+    carne.innerHTML = ('<img src="./images/icone_carne.png" alt="Ícone de carne">'+"A quantidade de carne é:")
     let agua = document.getElementById("agua")
     agua.style.display = "inline-block"
-    agua.innerHTML = ("A quantidade de agua é:")
+    agua.innerHTML = ('<img src="./images/icone_agua.png" alt="Ícone de agua">'+"A quantidade de agua é:")
     let refri = document.getElementById("refri")
     refri.style.display = "inline-block"
-    refri.innerHTML = ("A quantidade de refrigerante é:")
+    refri.innerHTML = ('<img src="./images/icone_refrigerante.png" alt="Ícone de refrigerante">'+"A quantidade de refrigerante é:")
     let cerveja = document.getElementById("cerveja")
     cerveja.style.display = "inline-block"
-    cerveja.innerHTML = ("A quantidade de cerveja é:")
+    cerveja.innerHTML = ('<img src="./images/icone_cerveja.png" alt="Ícone de cerveja">'+"A quantidade de cerveja é:")
 }
 
 function atualizarPag(){
